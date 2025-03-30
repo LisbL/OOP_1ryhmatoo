94b1f0d7cd4244973c8ac7becbe247d676d1a38e
LisbL <LisbethLepp12@gmail.com>
2025-03-30 08:37:33 +0300

    Midagi midagi

--- test.java
+++ test.java
@@ -1,3 +1,20 @@
+/*
+- Programm käsitleb mingit (inimlikku) tegevust (mängimist, kliendile vastamist,
+kodumasina kasutamist).
+- Programm peab kasutajalt midagi küsima, eeldades et kasutaja sisestab
+vastuse nõutud kujul.
+- Programm peaks olema kasutatav ilma, et programmi kohta oleks
+erilisi eelteadmisi. Küsimused peavad vajaliku info andma.
+Annab vajaliku üldtutvustava lühiinfo.
+- Programm peab sisaldama juhusliku suuruse kasutamist (klassi Random abil).
+- Programm peab koosnema mitmest klassist (sh. peaklass).
+Andmete kasutamine peaks käima erinevate objektide abil.
+Selleks loodud klassid peaksid sisaldama isendivälju, konstruktoreid,
+get- ja set-meetodeid ja teisi vajalikke meetodeid.
+- Programm peab olema mõistlikult kommenteeritud.
+- Kasutajaga suhtlemine peaks olema väga elementaarselt kujundatud.
+ */
+
 public class test {
     public static void main(String[] args) {
         System.out.println("mmm Monke");
